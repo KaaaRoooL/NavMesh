586b2e40aba8490697432e8f4990365048e332a8
Carolina Diaz <cadigo1992@gmail.com>
2022-05-20 20:09:16 +0200

    El perseguidor persigue al agente con límite de distancia

--- Assets/Scripts/AgentRaycast.cs
+++ Assets/Scripts/AgentRaycast.cs
@@ -4,13 +4,23 @@ using UnityEngine.AI;
 public class AgentRaycast : MonoBehaviour
 {
     public float distanceRay;
+    public Transform player;
+
+    public Transform puntoInicial;
+
+    NavMeshAgent perseguidor;
+
+    void Start()
+    {
+        perseguidor = GetComponent<NavMeshAgent>();
+    }
     //Vector3 directionToTarget = (player.position - Transform,position).normalized;
     void FixedUpdate()
     {
         RaycastHit hit;
 
         //if(Physics.Raycast(transform.position + vector3.up, directionToTarger, out hitInfo, distanceToPlayer, -1. QueryTriggerInteraction.Ignore))
-        
+        float distanceToPlayer = Vector3.Distance(player.position, transform.position);
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
@@ -18,6 +28,12 @@ public class AgentRaycast : MonoBehaviour
         else
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * distanceRay, Color.white);
+        }
+
+        if(distanceToPlayer < distanceRay){
+            perseguidor.destination = player.position;
+        } else {
+            perseguidor.destination = puntoInicial.position;
         }
     }
 }
