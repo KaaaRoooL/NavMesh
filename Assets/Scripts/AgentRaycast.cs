208cfcfa45167b8c7a6f8c0051baab9b21ff09ee
Carolina Diaz <cadigo1992@gmail.com>
2022-05-23 21:11:50 +0200

    El persiguidor no ve al agente si hay obtáculos

--- Assets/Scripts/AgentRaycast.cs
+++ Assets/Scripts/AgentRaycast.cs
@@ -9,31 +9,40 @@ public class AgentRaycast : MonoBehaviour
     public Transform puntoInicial;
 
     NavMeshAgent perseguidor;
+    RaycastHit hit;
 
     void Start()
     {
         perseguidor = GetComponent<NavMeshAgent>();
     }
-    //Vector3 directionToTarget = (player.position - Transform,position).normalized;
+    
     void FixedUpdate()
     {
-        RaycastHit hit;
-
-        //if(Physics.Raycast(transform.position + vector3.up, directionToTarger, out hitInfo, distanceToPlayer, -1. QueryTriggerInteraction.Ignore))
-        float distanceToPlayer = Vector3.Distance(player.position, transform.position);
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+        
+        //Vector3 directionToTarget = (player.position - transform.position).normalized;
+        //if(Physics.Raycast(transform.position + vector3.up, directionToTarger, out hitInfo, distanceToPlayer, -1, QueryTriggerInteraction.Ignore))
+        
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity,-1, QueryTriggerInteraction.Ignore) )
         {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.green);           
         }
         else
         {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * distanceRay, Color.white);
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * distanceRay, Color.red);  
         }
 
-        if(distanceToPlayer < distanceRay){
+        DistancePlayerRay();
+
+        
+       
+    }
+
+    private void DistancePlayerRay(){
+        float distanceToPlayer = Vector3.Distance(player.position, transform.position);
+        if(distanceToPlayer < distanceRay && hit.collider.tag == "Player"){
             perseguidor.destination = player.position;
         } else {
             perseguidor.destination = puntoInicial.position;
-        }
+        } 
     }
 }
